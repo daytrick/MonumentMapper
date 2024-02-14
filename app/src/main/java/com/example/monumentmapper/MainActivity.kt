bda4a4f41b71e579c7652d790901354b81e3d62d
daytrick <120808108+daytrick@users.noreply.github.com>
2024-02-14 17:08:37 +0000

    Got login _button_ to appear in nav bar
    Doesn't do anything yet tho

--- app/src/main/java/com/example/monumentmapper/MainActivity.kt
+++ app/src/main/java/com/example/monumentmapper/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity(), MapListener, LocationListener {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_account
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
