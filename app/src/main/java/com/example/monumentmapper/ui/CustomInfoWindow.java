55085cf6831e693dcaac7d3690bec32f2166715f
daytrick <120808108+daytrick@users.noreply.github.com>
2024-02-23 17:00:53 +0000

    Added (non-functional) camera button to InfoWindows

--- app/src/main/java/com/example/monumentmapper/ui/CustomInfoWindow.java
+++ app/src/main/java/com/example/monumentmapper/ui/CustomInfoWindow.java
@@ -3,9 +3,13 @@ package com.example.monumentmapper.ui;
 import com.example.monumentmapper.R;
 
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.infowindow.InfoWindow;
 import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
 
+/**
+ * Extend MarkerInfoWindow to look nicer.
+ *
+ * Idea from: https://stackoverflow.com/a/66195184
+ */
 public class CustomInfoWindow extends MarkerInfoWindow {
 
     /**
